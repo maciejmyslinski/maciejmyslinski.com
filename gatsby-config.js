dc654fffd0d9ab86d52df1b813cc1fc7368bf6ea
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-01-27 00:42:06 +0100

    Format gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-manifest', 'gatsby-plugin-offline', 'gatsby-plugin-react-helmet', 'gatsby-plugin-react-next', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-manifest',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-styled-components',
+  ],
 }
