80e95c810d4d4e1a4ec039bd9102d5a1a696222b
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-01-27 00:45:04 +0100

    Use helmet to add title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
-const TemplateWrapper = ({ children }) => <div>{children()}</div>
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet>
+      <html lang="en" />
+      <title>Maciej Myśliński - Front-end Develope at Netguru</title>
+    </Helmet>
+    {children()}
+  </div>
+)
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func.isRequired,
