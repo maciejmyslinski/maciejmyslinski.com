33f59caf2b6d4b8b44994f535a04e6a33c629122
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-01-26 23:42:00 +0100

    prettier

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    {children()}
-  </div>
-)
+const TemplateWrapper = ({ children }) => <div>{children()}</div>
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func.isRequired,
