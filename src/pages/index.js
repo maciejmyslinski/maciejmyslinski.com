4edf66d4ed9c8b4cfb07b78f8f1351f4074bd1ab
Maciej Myśliński <maciej.myslinski@netguru.pl>
2018-01-30 00:56:27 +0100

    Add content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-  </div>
-)
+	<div>
+		<h1>Why do I get up every mornging</h1>
+		<p>
+			To live a valuable and a rewarding life by showing <b>love</b> to God Jehovah, myself, and other people. To{' '}
+			<b>improve myself</b> so that I'm more useful, more powerful, and <b>freer</b> person, putting myself in the
+			best position possible to <b>give value</b> and practical help to other people.
+		</p>
+		<h2>What do I do the whole day?</h2>
+		<ul>
+			<li>
+				<b>Help people have a better job</b>. Since February 2017 I took part in the recruitment of over 30
+				people at @Netguru.
+			</li>
+			<li>
+				<b>Make people better React developers.</b> I mentor new hires and regularly share knowledge about
+				front-end development during company-wide meetings.
+			</li>
+			<li>
+				<b>Improve the quality of software</b> created at @Netguru. While developing a solution, I reflect on
+				how it will influence conversion rates, revenue, and user experience. What parts of software development
+				are my favorite ones? Those that benefit my team the most - writing tests and doing/receiving a code
+				review.️
+			</li>
+			<li>
+				<b>Contribute to Open-Source.</b> I have a great amount of respect for people who maintain open-source
+				libraries. Thus, I regularly seek possibilities to contribute.
+			</li>
+			<li>
+				<b>Strive to be a husband my wife deserves.</b> She says I’m doing quite well with this one 😉
+			</li>
+			<li>Well, I do a bunch of other stuff. You can ask me about it over a 🍺 or two.</li>
+		</ul>
+		<h2>Where can you find me?</h2>
+		<ul>
+			<li>Medium</li>
+			<li>Linkedin</li>
+			<li>Twitter</li>
+			<li>Email</li>
+			<li>Github</li>
+		</ul>
+		<p>Oh, and by the way. That's me:</p>
+	</div>
+);
 
-export default IndexPage
+export default IndexPage;
