d835f0102fa09f7d65ea12d2c918eb42b12563fe
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-08-26 23:47:23 +0200

    Add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  plugins: ['gatsby-plugin-offline'],
+  plugins: [
+    'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Maciej Myśliński`,
+        short_name: `Maciej Myśliński`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#2D9CDB`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+  ],
 };
