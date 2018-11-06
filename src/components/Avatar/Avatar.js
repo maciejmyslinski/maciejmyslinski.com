67d3f1b24fdce6666a0251b7508edf20d65b2839
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-11-06 19:18:28 +0100

    Scale avatar better

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,13 +1,24 @@
 import system from 'system-components';
 import avatar from '../../images/avatar.png';
+import { cssLocks } from '../../utils/cssLocks';
 
-export const Avatar = system({
-  is: 'img',
-  width: '9vw',
-  borderRadius: '9vw',
-  mt: '5rem',
-  m: '0 auto',
-});
+export const Avatar = system(
+  {
+    is: 'img',
+    width: '10rem',
+    borderRadius: '100vw',
+    mt: '5rem',
+    m: '0 auto',
+  },
+  ({ theme }) => ({
+    [`@media (min-width: ${theme.breakpoints[0]})`]: {
+      width: cssLocks({ minSize: 10, maxSize: 14 }),
+    },
+    [`@media (min-width: ${theme.breakpoints[1]})`]: {
+      width: '14rem',
+    },
+  })
+);
 
 Avatar.defaultProps = {
   ...Avatar.defaultProps,
